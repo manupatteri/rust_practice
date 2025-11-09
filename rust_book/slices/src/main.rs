9e9da364403cfa0a27c56fe56b424329e0c57bd2
Manoj Narayanan <manoj.narayanan@gmail.com>
2025-11-09 06:19:40 -0800

    slices

--- rust_book/slices/src/main.rs
+++ rust_book/slices/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("Hello, world!");
+    println!("{}", r_first_word(&String::from("asdfg rty")));
     println!("{}", first_word(&String::from("asd")));
     println!("{}", get_first_word(&String::from("asd")));
 }
@@ -22,4 +23,14 @@ fn first_word(s: &String) -> usize {
     }
 
     s.len()
+}
+fn r_first_word(full_string: &String) -> usize {
+	let word_bytes = full_string.as_bytes();
+	for (i, &item) in word_bytes.iter().enumerate() {
+		if item == b' ' {
+			return i;
+		}
+	}
+	word_bytes.len()
+	
 }
