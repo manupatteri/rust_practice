88053db76f42d299d61adfa7d2d7f7c395c937f1
Manoj Narayanan <manoj.narayanan@gmail.com>
2025-12-26 01:38:44 -0800

    comments - A very simple project

---
+++ rust_book/comments/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    //Single line comment
+    println!("Hello, world!"); //same line comment
+    /*Multi line comment
+    Spanning multiple lines
+    which keeps on going 
+    and going */
+    println!("Hello, world!"); //same line comment
+    
+}
