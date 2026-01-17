db180b147497456685859c6ed252a989c2f00fef
Manoj Narayanan <manoj.narayanan@gmail.com>
2026-01-16 18:25:07 -0800

    rust_book: ownership practice

--- rust_book/ownership/src/main.rs
+++ rust_book/ownership/src/main.rs
@@ -7,4 +7,40 @@ fn main() {
     println!("{s2} world");
     s1 = s2.clone();
     println!("{s1} {s2} ");
+    let mut only_variable = String::from("First");
+    only_variable = String::from("Second");
+
+    //At this point, nothing is referring to the original value("First") on the heap at all. 
+    //The original string thus immediately goes out of scope. Rust will run the drop function on it and its memory will be freed right away. 
+    //When we print the value at the end, it will be "Second".
+
+    println!("{only_variable}");
+    let x : i32 = 5;
+    let y = x;
+    println! ("First Var {x} is still valid while new one {y} got initialized with same value");
+    takes_ownership(only_variable);
+    //error[E0382]: borrow of moved value: `only_variable`
+    //  --> src/main.rs:22:31
+    //   |
+    //10 |     let mut only_variable = String::from("First");
+    //   |         ----------------- move occurs because `only_variable` has type `String`, which does not implement the `Copy` trait
+    //...
+    //21 |     takes_ownership(only_variable);
+    //   |                     ------------- value moved here
+    //22 |     println! ("only_variable {only_variable} no more valid here ");
+    //   |                               ^^^^^^^^^^^^^ value borrowed here after move
+    //   |
+
+    //println! ("only_variable {only_variable} no more valid here ");
+    let num = 4;
+    makes_copy(num);
+    println! ("scalar can be used after being used function use: {num}");
+
+}
+fn takes_ownership(some_string : String) {
+    println!("some_string:{some_string} moved here inside Fn takes_ownership");
+}//some_string goes out of scope and drop getting called.
+
+fn makes_copy(some_integer: i32) {
+    println!("Copying scalar type:{some_integer}");
 }
